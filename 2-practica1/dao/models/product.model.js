14bcb49a0fba1ea6dabec3797181c70ed0e5a03b
Facundo Castellano <castellanofacundo@gmail.com>
2023-08-07 23:00:48 -0300

    con las rutas de get y post de /api/products

--- 2-practica1/dao/models/product.model.js
+++ 2-practica1/dao/models/product.model.js
@@ -1,4 +1,6 @@
 const { Schema, model } = require("mongoose");
+const { v4: uuid } = require("uuid");
+
 
 const schema = new Schema(
   {
@@ -6,6 +8,7 @@ const schema = new Schema(
     description: { type: String, required: true },
     price: { type: Number, required: true },
     stock: { type: Number, default: 0 },
+    code: { type: String, unique: true, default: uuid },
     category: { type: String, required: true },
     status: { type: Boolean, default: true },
     thumbnails: {
@@ -22,3 +25,5 @@ const schema = new Schema(
 
 const productModel = model("Product", schema);
 module.exports = productModel;
+//Nota en el code, el valor por default tiene qus eser uudi, para pasar la funcion, de esta manera  mongoose la ejecutara para cada documento que necesite usar el default.
+//si le asigno uuid(), le estoy diciendo al esquema que el valor por defecto es el valor retornado por uuid() cuando crea el esquema.. es decir todos van a tener un mismo codigo uuid por defecto, esto esta mal. 
--- 2-practica1/dao/product.manager.js
+++ 2-practica1/dao/product.manager.js
@@ -1,40 +1,41 @@
 const productModel = require("./models/product.model");
-const db = require("../config/connection.mongo");
-
-
+//const db = require("../config/connection.mongo");
 class ProductManager {
-  createProduct({
-    title,
-    description,
-    price,
-    stock,
-    category,
-    status,
-    thumbnails,
-  }) {
-    productModel.create({
-      title,
-      description,
-      price,
-      stock,
-      category,
-      status,
-      thumbnails,
-    });
+  async addProduct(product) {
+    return await productModel.create(product);
+  }
+
+  async getAll({limit=undefined,page=1}) {
+    console.log(limit,page)
+    if(!limit){
+      return await productModel.find({})
+    }else {
+      return await productModel.find({}).limit(limit).skip((page-1)*limit)
+    }
+    
+  }
+
+  async getById(id) {
+    return await productModel.findOne({ _id: id });
+  }
+
+  async updateById(id, productUpdated) {
+    return await productModel.findOneAndUpdate(id, productUpdated);
+  }
+
+  async deleteById(id) {
+    return await productModel.deleteOne({ _id: id });
   }
 }
 
-
 const p = {
-  title:"producto 1",
+  title: "producto 1",
   description: "la descripcion del producto1",
   price: 50,
   stock: 10,
-  category:"juguete",
+  category: "juguete",
   status: true,
-  thumbnails:["path/img1.jpg","path/img2.jpg"],
-}
+  thumbnails: ["path/img1.jpg", "path/img2.jpg"],
+};
 
-const PM = new ProductManager()
-
-PM.createProduct(p)
+module.exports = new ProductManager(); //singleton --> siempre exporto una misma instancia de clase.
