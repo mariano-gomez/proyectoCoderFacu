2826363eb9a5aad0774282d44bc0009ae0d8433d
Facundo Castellano <castellanofacundo@gmail.com>
2023-10-05 16:03:56 -0300

    se agrego el controlador del carrito y se refactorizo el ruteo

---
+++ 4-entrega3/controllers/carts.controller.js
@@ -0,0 +1,224 @@
+const { factoryManager } = require('../config/process.config')
+
+const userManager = factoryManager.userManager
+const cartManager = factoryManager.cartManager
+const productManager = factoryManager.productManager
+const ticketManager = factoryManager.ticketManager
+
+class CartController {
+  //funcion para ruta 1  router.post('/')
+  static async create(req = request, res) {
+    try {
+      const { userId } = req.query
+      //aca podria ver si ese usuario ya tiene un carrito, y si lo tiene no lo creo y traigo el ya creado.
+
+      if (!userId) {
+        throw new Error('userId must be a query param')
+      }
+
+      await cartManager.add({ userId })
+      console.log('se creo un carrito')
+      res.send({ status: 'Success, a new was created' })
+    } catch (e) {
+      console.log(e)
+      res.status(500).send({ status: 'Error', Error: e.message })
+    }
+  }
+  //funcion para ruta 2    router.get('/:cid')
+  static getProducts = async (req, res) => {
+    try {
+      const cartId = req.params.cid
+      //const cart = await cartManager.getById(cartId)
+      const cart = await cartManager.getByIdProductsPopulate(cartId)
+      if (!cart) {
+        throw new Error('cart not found')
+      }
+      res.send({
+        status: 'success',
+        payload: { user: cart.user, products: cart.products },
+      })
+    } catch (e) {
+      res.send({ status: 'Error', Error: e.message })
+    }
+  }
+
+  //funcion para ruta 3    router.post('/:cid/product/:pid')
+  //agrego un producto a un carrito, puedo pasar el qty y si no lo paso por default sera 1.
+  static addProduct = async (req, res) => {
+    try {
+      const id = req.params.cid //es el id del cart
+      const productId = req.params.pid
+      const qty = req.query.qty
+      const wasAdded = await cartManager.getByIdAndAddProduct({
+        id,
+        productId,
+        qty,
+      })
+      if (wasAdded) {
+        res.send({
+          status: 'Success',
+          payload: {
+            operation: 'add product to a cart',
+            cart: id,
+            product: productId,
+            quantityAdded: qty,
+          },
+        })
+      }
+    } catch (e) {
+      console.log(e)
+      res.send({ status: `Error`, Error: e.message })
+    }
+  }
+
+  //funcion para ruta 4    router.put('/:cid/product/:pid')
+  //seteo el product.qty, en un valor que recibo por body.
+  //Uso put por la consigna pero seria más adecuado es un metodo patch.
+
+  static setProductQty = async (req, res) => {
+    try {
+      const id = req.params.cid //es el id del cart
+      const productId = req.params.pid
+      const qty = +req.body.qty
+      await cartManager.getByIdAndModifyProductQty({
+        id,
+        productId,
+        qty,
+      })
+
+      res.send({
+        status: 'success',
+        payload: {
+          operation: 'Update quantity of a sigle product in cart',
+          cart: id,
+          product: productId,
+          quantity: qty,
+        },
+      })
+    } catch (e) {
+      console.log(e)
+      res.send({ status: `Error`, Error: e.message })
+    }
+  }
+
+  //funcion para ruta 5    router.delete('/:cid/product/:pid')
+  //ruta para eliminar un producto de un carrito. (elimina todas las cantidades que haya del mismo)
+  static deleteProduct = async (req, res) => {
+    try {
+      const id = req.params.cid //es el id del cart
+      const productId = req.params.pid
+      await cartManager.deleteProduct({
+        id,
+        productId,
+      })
+
+      res.status(200).send({
+        status: 'success',
+        payload: {
+          operation: 'delete product from cart',
+          cart: id,
+          product: productId,
+        },
+      })
+    } catch (e) {
+      console.log(e)
+      res.send({ status: `Error`, Error: e.message })
+    }
+  }
+
+  //funcion para ruta 6    router.delete('/:cid')
+  //ruta para vaciar un carrito (elimina todos los productos)
+  static clear = async (req, res) => {
+    try {
+      const id = req.params.cid //es el id del cart
+      await cartManager.clearProducts({
+        id,
+      })
+
+      res.status(200).send({
+        status: 'success',
+        payload: {
+          operation: 'delete all products from cart',
+          cart: id,
+        },
+      })
+    } catch (e) {
+      console.log(e)
+      res.send({ status: `Error`, Error: e.message })
+    }
+  }
+
+  //funcion para ruta 7    router.put('/:cid')
+  //esta ruta me hace un update de todos los productos (los cambio a todos, segun lo q recibo, incluido las qty)
+  static updateProducts = async (req, res) => {
+    const id = req.params.cid //es el id del cart
+    const products = req.body
+    await cartManager.setAllProducts({ id, productsUpdated: products })
+    res.status(200).send({
+      status: 'success',
+      payload: {
+        operation: 'set all products from cart',
+        cart: id,
+        products,
+      },
+    })
+  }
+
+  //funcion para ruta 8    router.get('/:cid/purchase')
+  //esta ruta me efectua la compra de los productos en el carrito.
+  static makePurchase = async (req, res) => {
+    const cid = req.params.cid //es el id del cart
+    const cart = await cartManager.getById(cid)
+    const { email: purchaser } = await userManager.getById(cart.user)
+    //const products = cart.products
+    const concept = []
+    const notEnoughtProducts = []
+    let amount = 0
+    let status
+
+    for (const index in cart.products) {
+      const { product: pid, qty } = cart.products[index]
+      const product = await productManager.getById(pid)
+      const { stock, price } = product
+
+      if (stock >= qty) {
+        amount += qty * price
+        product.stock -= qty
+        concept.push({
+          product: product.title,
+          price,
+          units: qty,
+          subtotal: qty * price,
+        })
+        cartManager.deleteProduct({ id: cid, productId: pid })
+        product.save()
+      } else {
+        notEnoughtProducts.push({
+          title: product.title,
+          stock,
+          demannding: qty,
+          pid: product.id.toString(),
+        })
+      }
+    }
+    if (amount) {
+      ticketManager.add({ purchaser, amount, concept })
+      status = 'success'
+      if (notEnoughtProducts.length) {
+        res.send({ status, purchaser, notBuyed: notEnoughtProducts })
+        return
+      }
+    } else {
+      status = 'error'
+    }
+    res.send({ status, purchaser })
+  }
+}
+
+//cuando un user no logueado toque el boton carts, del navBar, va aentrar aca y se lo va a redicreccionar automatciamente.
+// //con cuando un user logueado tocque ese boton va a entrar a carts/:cid
+// router.get('/', async (req, res) => {
+//   res.send('ruta vacia')
+// })
+
+module.exports = CartController
--- 4-entrega3/routes/api/carts.router.js
+++ 4-entrega3/routes/api/carts.router.js
@@ -5,6 +5,7 @@ const cartManager = factoryManager.cartManager
 const productManager = factoryManager.productManager
 const ticketManager = factoryManager.ticketManager
 const RoutePolices = require('../../middelwares/routes.polices')
+const CartController = require('../../controllers/carts.controller')
 //const {onlyAdmin,onlyUser} = require('../../middelwares/routes.polices')
 //const myProducts = new ProductManager("products.json");
 
@@ -14,149 +15,28 @@ const router = Router() //este objeto contendra todas las rutas de esta seccion,
 // TODOAS LAS RUTAS QUE SIGUEN tienen por defecto el prefijo "/api/carts"
 
 //ruta 1, crea un carrito nuevo (pero tengo que asociarlo a un userId)
-router.post('/', async (req = request, res) => {
-  try {
-    const { userId } = req.query
-    //aca podria ver si ese usuario ya tiene un carrito, y si lo tiene no lo creo y traigo el ya creado.
+router.post('/', CartController.create)
 
-    if (!userId) {
-      throw new Error('userId must be a query param')
-    }
+router.get('/:cid', CartController.getProducts)
 
-    await cartManager.add({ userId })
-    console.log('se creo un carrito')
-    res.send({ status: 'Success, a new was created' })
-  } catch (e) {
-    console.log(e)
-    res.status(500).send({ status: 'Error', Error: e.message })
-  }
-})
+router.post('/:cid/product/:pid',RoutePolices.onlyUser,CartController.addProduct)
 
-//cuando un user no logueado toque el boton carts, del navBar, va aentrar aca y se lo va a redicreccionar automatciamente.
-//con cuando un user logueado tocque ese boton va a entrar a carts/:cid
-router.get('/', async (req, res) => {
-  res.send('ruta vacia')
-})
+router.put('/:cid/product/:pid',CartController.setProductQty)
 
-//ruta 2, devuelvo los productos de un carrito en especifico.
-router.get('/:cid', async (req, res) => {
-  try {
-    const cartId = req.params.cid
-    //const cart = await cartManager.getById(cartId)
-    const cart = await cartManager.getByIdProductsPopulate(cartId)
-    if (!cart) {
-      throw new Error('cart not found')
-    }
-    res.send({
-      status: 'success',
-      payload: { user: cart.user, products: cart.products },
-    })
-  } catch (e) {
-    res.send({ status: 'Error', Error: e.message })
-  }
-})
+router.delete('/:cid/product/:pid',CartController.deleteProduct)
 
-//agrego un producto a un carrito, puedo pasar el qty y si no lo paso por default sera 1.
-router.post('/:cid/product/:pid', RoutePolices.onlyUser, async (req, res) => {
-  try {
-    const id = req.params.cid //es el id del cart
-    const productId = req.params.pid
-    const qty = req.query.qty
-    const wasAdded = await cartManager.getByIdAndAddProduct({
-      id,
-      productId,
-      qty,
-    })
-    if (wasAdded) {
-      res.send({
-        status: 'Success',
-        payload: {
-          operation: 'add product to a cart',
-          cart: id,
-          product: productId,
-          quantityAdded: qty,
-        },
-      })
-    }
-  } catch (e) {
-    console.log(e)
-    res.send({ status: `Error`, Error: e.message })
-  }
-})
+router.delete('/:cid',CartController.clear)
 
-//seteo el product.qty, en un valor que recibo por body.
-//Uso put por la consigna pero seria más adecuado es un metodo patch.
+//esta ruta me hace un update de todos los productos (los cambio a todos, segun lo q recibo, incluido las qty)
+router.put('/:cid',CartController.updateProducts)
 
-router.put('/:cid/product/:pid', async (req, res) => {
-  try {
-    const id = req.params.cid //es el id del cart
-    const productId = req.params.pid
-    const qty = +req.body.qty
-    await cartManager.getByIdAndModifyProductQty({
-      id,
-      productId,
-      qty,
-    })
+router.get('/:cid/purchase',CartController.makePurchase)
 
-    res.send({
-      status: 'success',
-      payload: {
-        operation: 'Update quantity of a sigle product in cart',
-        cart: id,
-        product: productId,
-        quantity: qty,
-      },
-    })
-  } catch (e) {
-    console.log(e)
-    res.send({ status: `Error`, Error: e.message })
-  }
-})
 
-//ruta para elminar un producto de un carrito. (elimina todas las cantidades que haya del mismo)
-router.delete('/:cid/product/:pid', async (req, res) => {
-  try {
-    const id = req.params.cid //es el id del cart
-    const productId = req.params.pid
-    await cartManager.deleteProduct({
-      id,
-      productId,
-    })
 
-    res.status(200).send({
-      status: 'success',
-      payload: {
-        operation: 'delete product from cart',
-        cart: id,
-        product: productId,
-      },
-    })
-  } catch (e) {
-    console.log(e)
-    res.send({ status: `Error`, Error: e.message })
-  }
-})
 
-//ruta para vaciar un carrito (elimina todos los productos)
-router.delete('/:cid', async (req, res) => {
-  try {
-    const id = req.params.cid //es el id del cart
-    await cartManager.clearProducts({
-      id,
-    })
 
-    res.status(200).send({
-      status: 'success',
-      payload: {
-        operation: 'delete all products from cart',
-        cart: id,
-      },
-    })
-  } catch (e) {
-    console.log(e)
-    res.send({ status: `Error`, Error: e.message })
-  }
-})
+
 
 //esta ruta me hace un update de todos los productos (los cambio a todos, segun lo q recibo, incluido las qty)
 router.put('/:cid', async (req, res) => {
