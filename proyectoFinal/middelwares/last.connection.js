3e4f71207c958119ec9d40ecde08d4cac0d6ab8f
Facundo Castellano <castellanofacundo1991@gmail.com>
2023-12-12 16:11:21 -0300

    se eliminaron todos las entregas anteriores

--- proyectoFinal/middelwares/last.connection.js
+++ proyectoFinal/middelwares/last.connection.js
@@ -2,7 +2,6 @@ const userManager = require('../dao/mongo/user.manager')
 
 const setLastConnection = async (req, res, next) => {
   try {
-    console.log('seteando fecha a: ', Date.now())
     await userManager.updateById(req.user.id, { last_connection: Date.now() })
     next()
   } catch (err) {
