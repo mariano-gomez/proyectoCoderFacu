212a073cd6e17bc84804e46eb1fdd2fbe758b96f
Facundo Castellano <castellanofacundo1991@gmail.com>
2023-12-13 13:24:20 -0300

    ver

--- 5-entrega4/middelwares/logger.http.js
+++
@@ -1,8 +0,0 @@
-const logger = require('../logger')
-
-const httpLogger = (req, _res, next) => {
-  logger.http(`[${req.method}] - ${req.url} Date:${(new Date()).toISOString()}`)
-  next()
-}
-
-module.exports = httpLogger
--- 5-entrega4/routes/test/test.logger.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const TestController = require('../../controllers/test.controller')
-
-const router = Router() 
-
-router.get('/logger', TestController.logger)
-
-module.exports = router
